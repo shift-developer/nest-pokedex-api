dfa66ffb7b2b3e2b5b7a749e661311ba4684b546
Juan Cruz Gonzalez <juancruz.gonfer@gmail.com>
2022-09-04 19:45:19 -0300

    feature: static content module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
